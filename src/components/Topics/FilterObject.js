c96b3d395c17e88eb7b4e7063b646fc7e805b40e
Richard Miller <richardmmill@gmail.com>
2021-03-03 11:27:20 -0500

    Richards work on Lab 3.2

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -7,7 +7,7 @@ class FilterObject extends Component {
             unFilteredArray: [
                 { "name": "Jimmy Joe", "title": "Hack0r", "age": 12 },
                 { "name": "Jeremy Schrader", "age": 24, "hairColor": "brown" },
-                { "name": "Carly Armstrong", "title": "CEO" }
+                { "name": "Carly Armstrong", "title": null }
             ],
             userInput: '',
             filterArray: []
@@ -18,26 +18,27 @@ class FilterObject extends Component {
         this.setState({ userInput: value })
     }
 
-    // filterObjArray(userInput) {
-    //     let arr = this.state.unFilteredArray
-    //     console.log(arr)
-    //     let newFilteredArr = arr.filter((elem, i) => {
-    //         return elem[i] === userInput
-    //     })
-    //     console.log(newFilteredArr)
-    //     this.setState({ filteredArray: newFilteredArr })
-    // }
-
     filterObjArray(userInput) {
-        let arr = this.state.unFilteredArray
-        let newFilteredArr = []
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i].hasOwnProperty(userInput)) {
-                newFilteredArr.push(arr[i])
-            }
-        }
+        console.log(userInput)
+        let arr = this.state.unFilteredArray.slice()
+        console.log(arr)
+        let newFilteredArr = arr.filter((elem) => {
+            return elem.hasOwnProperty(userInput)
+        })
+        console.log(newFilteredArr)
         this.setState({ filterArray: newFilteredArr })
     }
+
+    // filterObjArray(userInput) {
+    //     let arr = this.state.unFilteredArray
+    //     let newFilteredArr = []
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (arr[i].hasOwnProperty(userInput)) {
+    //             newFilteredArr.push(arr[i])
+    //         }
+    //     }
+    //     this.setState({ filterArray: newFilteredArr })
+    // }
 
 
 
@@ -50,7 +51,7 @@ class FilterObject extends Component {
                 <span className='puzzleText'>Original: {JSON.stringify(this.state.unFilteredArray, null, 10)}</span>
                 <input className='inputLine' onChange={event => this.handleInput(event.target.value)}></input>
                 <button className='confirmationButton' onClick={() => { this.filterObjArray(this.state.userInput) }}>Filter</button>
-                <span className='resultsBox filterObjectRB'>Filtered: {this.state.filterArray}</span>
+                <span className='resultsBox filterObjectRB'>Filtered: {JSON.stringify(this.state.filterArray)}</span>
             </div >
         )
     }
--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 
 class FilterString extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            unFilteredArray: ['James', 'Jessica', 'Melody', 'Tyler', 'Blake', 'Jennifer', 'Mark', 'Maddy'],
+            userInput: '',
+            filteredArray: []
+        }
+
+    }
+
+    handleInput(value) {
+        this.setState({ userInput: value })
+    }
+
+    filterString(userInput) {
+        let arr = this.state.unFilteredArray.slice()
+        let newFilteredArr = arr.filter(elem => {
+            return elem === userInput
+        })
+        console.log(newFilteredArr)
+        this.setState({ filteredArray: newFilteredArr })
+    }
+
+
+
+
     render() {
         // console.log(this.state);
         return (
-            <p>FilterString Component</p>
+            <div className='puzzleBox filterStringPB'>
+                <h4>Filter String</h4>
+                <span className='puzzleText'>Original: {JSON.stringify(this.state.unFilteredArray)}</span>
+                <input className='inputLine' onChange={event => this.handleInput(event.target.value)}></input>
+                <button className='confirmationButton' onClick={() => { this.filterString(this.state.userInput) }}>Filter</button>
+                <span className='resultsBox filterStringRB'>Filtered: {JSON.stringify(this.state.filteredArray)}</span>
+            </div>
+
         )
     }
 };
